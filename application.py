f243e14e62c463583e630fc28fb6e2f693117ce6
fidiego <cognitivereflex@gmail.com>
2014-12-07 15:38:05 -0500

    minor updates

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 
 # SET UP ENVIRONMENT
 from common_functions import read_env, env_var
@@ -13,10 +13,11 @@ from flask import render_template
 app = Flask(__name__)
 
 if env_var('ENVIRONMENT') == 'LOCAL':
+    sys.stdout.write('\033[0:31em  >>  Running on LOCAL environment \033[0:0em')
     app.debug=True
 
 @app.route('/')
-def hello():
-    return render_template('hello_world.html', message="hello world")
+def home():
+    return render_template('hello_world.html', message="Welcome to *")
 
 
